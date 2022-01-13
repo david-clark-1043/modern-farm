4fb67d483d146cac648681dfcbb146c0688f635b
David Clark <david.clark.1043@gmail.com>
2022-01-13 14:48:16 -0600

    Chapter 1 code - imports from plan.js and invokes function in main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,6 @@
-console.log("Welcome to the main module")
+import { createPlan } from "./plan.js"
 
+console.log("Welcome to the main module");
+
+const yearlyPlan = createPlan();
+console.log(yearlyPlan);
